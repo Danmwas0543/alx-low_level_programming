b4c69e33a249b070c5527f96baa94c754b273856
daniel <danielmwangi12244@gmai.com>
2022-12-14 07:39:44 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main  point
- * return: 0
+ * return: o
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
