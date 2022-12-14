b0282b9b63914fdb1b88f675989ef5dbd1a12ce0
daniel <danielmwangi12244@gmai.com>
2022-12-14 08:58:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using printf to print programs
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
