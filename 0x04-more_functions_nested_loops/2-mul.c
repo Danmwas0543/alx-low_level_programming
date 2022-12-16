f0d59e792f231f0043334856bc47fd9b37d34a76
daniel <danielmwangi12244@gmai.com>
2022-12-16 06:02:17 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplying two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
