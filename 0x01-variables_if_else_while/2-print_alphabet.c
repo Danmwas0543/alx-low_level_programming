ab3687686d002439811e7dcb39d233b095c65483
daniel <daniel.mwangi.com>
2023-01-01 05:56:46 -0800

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase
+ * Return: zero 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+		return (0);
+}
