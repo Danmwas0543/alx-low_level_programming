d32ef96ad1b309b265853f23ef53849d6f7be688
Danmwas0543 <danielmwangi12244>
2023-10-10 00:48:49 +0300

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main ->displays the file's filename.
+ * Return: 0 (success) 
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+Objective is to learn macros
