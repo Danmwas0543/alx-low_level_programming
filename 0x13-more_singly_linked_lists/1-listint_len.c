a0899b8a2add810c05acf0a9b2ed7ab51ae0451f
Danmwas0543 <danielmwangi12244>
2024-02-08 14:15:56 +0300

    task

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_t - elements in a linked list
+ *
+ * @b: head of a pointer
+ * return: the amount of elements
+ */
+size_t listint_len(const listint_t *h);
+{
+	size_t number = 0;
+	
+	while (b != NULL)
+	{
+		b = b->next;
+		number++
+	}
+	return (number);
+}
