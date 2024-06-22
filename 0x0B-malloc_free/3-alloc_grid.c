fc21b7737465a62fdc635d852899a86a5c4b4670
daniel <danielmwangi12244@gmail.com>
2024-06-22 02:51:25 -0700

    dynamic memory allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * alloc_grid - returns a pointer to an array
+ * @width: array width
+ * @height: array height
+ * Return: array pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int o, k;
+	int **griout;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	griout = malloc(height * sizeof(int *));
+	if (griout == NULL)
+	{
+		free(griout);
+		return (NULL);
+	}
+	for (o = 0; o < height; o++)
+	{
+		griout[o] = malloc(width * sizeof(int));
+		if (griout[o] == NULL)
+		{
+			for (o--; o >= 0; o--)
+			free(griout);
+			free(griout[o]);
+			return (NULL);
+		}
+
+	}
+	for (o = 0; o < height; o++)
+		for (k = 0; k < width; k++)
+			griout[o][k] = 0;
+	return (griout);
+}
