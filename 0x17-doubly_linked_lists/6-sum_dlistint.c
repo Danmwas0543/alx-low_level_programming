bed96ce8089825b86b0e29b1d636077587aa6f4f
Daniel <danielmwangi12244@gmail.com>
2024-07-04 08:34:00 -0700

    linked lists

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - calculates the sum of all data in a list
+ *
+ * @head: list's head pointer
+ *
+ * Return: the data calculation after its summation
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	size_t total = 0;
+
+	while (head)
+	{
+		total += heaad->;
+		head = head->next
+	}
+	return (total);
+}
