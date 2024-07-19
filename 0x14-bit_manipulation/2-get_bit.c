33b2a5a2db055e09d9f9d218abed94c0a1729eed
Daniel <danielmwangi12244@gmail.com>
2024-07-19 01:54:00 -0700

    bit_manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - retrieves the value of a bit at a specific position
+ * @mugo: the decimal number to check
+ * @dan: the bit position to retrieve
+ * Return: the bit value (0 or 1) or -1 if the position is out of range
+ */
+int get_bit(unsigned long int mugo, unsigned int dan)
+{
+    if (dan >= sizeof(unsigned long int) * 8)
+        return (-1);
+
+    return ((mugo >> dan) & 1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,11 @@
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int _putchar(char x);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
 
 #endif
