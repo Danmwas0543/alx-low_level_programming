ee59874dc330c1ea5b95a6ddc3726ba8c802d50b
Daniel <danielmwangi12244@gmail.com>
2024-07-19 02:14:35 -0700

    bit_manipulation

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,14 +2,14 @@
 
 /**
  * get_bit - retrieves the value of a bit at a specific position
- * @mugo: the decimal number to check
- * @dan: the bit position to retrieve
+ * @n: the decimal number to check
+ * @index: the bit position to retrieve
  * Return: the bit value (0 or 1) or -1 if the position is out of range
  */
-int get_bit(unsigned long int mugo, unsigned int dan)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (dan >= sizeof(unsigned long int) * 8)
-	    return (-1);
-	
-	return ((mugo >> dan) & 1);
+    if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
+
+    return ((n >> index) & 1);
 }
