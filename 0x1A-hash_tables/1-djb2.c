cbbd19825457f7a2944b2ef7cb4f82b5858d345c
Daniel <danielmwangi12244@gmail.com>
2024-07-24 05:27:15 -0700

    javascript

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Implements the djb2 algorithm for hashing.
+ * @str: Represents the string to hash.
+ *
+ * Return: Returns the computed hash value.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int danu;
+    int mugo;
+
+    danu = 5381;
+    while ((mugo = *str++))
+        danu = ((danu << 5) + danu) + mugo;
+
+    return (danu);
+}
